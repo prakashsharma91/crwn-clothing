1a4df0f41010acca2e20250959d111f3652b59a7
PrAkAsH <prakashsharmra91@gmail.com>
2020-04-18 01:51:01 +0530

    adding header and style

--- src/components/collection-preview/collection-preview.components.jsx
+++ src/components/collection-preview/collection-preview.components.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CollectionItem from '../collection-item/collection-item.component'
+import CollectionItem from "../collection-item/collection-item.component";
 
 import "./collection-preview.styles.scss";
 
@@ -7,13 +7,13 @@ const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.
-      filter((item, idx) => idx < 4)
-      .map(({ id, ...otherItemProps }) => (
-        <CollectionItem id={id} {...otherItemProps} />
-      ))}
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem id={id} {...otherItemProps} />
+        ))}
     </div>
   </div>
 );
 
-export default CollectionPreview
+export default CollectionPreview;

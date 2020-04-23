80f9a3705b200317aaa21ab8823a0d819de4729d
PrAkAsH <prakashsharmra91@gmail.com>
2020-04-24 02:03:28 +0530

    stripe integeration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_VmJGN5eD5rXm49bx8W01OSDO00lvjk4K7h";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image=""
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+
+export default StripeCheckoutButton
